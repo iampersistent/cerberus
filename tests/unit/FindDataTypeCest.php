7fcaaf03a5ba231f34e6e45af26e98c6e9f0b1f4
iampersistent <develop@zestic.com>
2016-12-21 10:41:22 -0800

    update find data type test to new enum

--- tests/unit/FindDataTypeCest.php
+++ tests/unit/FindDataTypeCest.php
@@ -1,37 +1,34 @@
 <?php
 declare(strict_types = 1);
 
-use Cerberus\Core\{CategoryType, FindDataType};
+use Cerberus\Core\{
+    FindDataType, Identifier
+};
 
 class FindDataTypeCest
 {
-    public function testArray(UnitTester $I)
-    {
-        $I->assertSame(CategoryType::ID_DATATYPE_ARRAY, FindDataType::handle(['data']));
-    }
-
     public function testBoolean(UnitTester $I)
     {
-        $I->assertSame(CategoryType::ID_DATATYPE_BOOLEAN, FindDataType::handle(false));
+        $I->assertSame(Identifier::DATATYPE_BOOLEAN, FindDataType::handle(false));
     }
 
     public function testDate(UnitTester $I)
     {
-        $I->assertSame(CategoryType::ID_DATATYPE_DATETIME, FindDataType::handle(new DateTime()));
+        $I->assertSame(Identifier::DATATYPE_DATETIME, FindDataType::handle(new DateTime()));
     }
 
     public function testDouble(UnitTester $I)
     {
-        $I->assertSame(CategoryType::ID_DATATYPE_DOUBLE, FindDataType::handle(3.14));
+        $I->assertSame(Identifier::DATATYPE_DOUBLE, FindDataType::handle(3.14));
     }
 
     public function testInteger(UnitTester $I)
     {
-        $I->assertSame(CategoryType::ID_DATATYPE_INTEGER, FindDataType::handle(42));
+        $I->assertSame(Identifier::DATATYPE_INTEGER, FindDataType::handle(42));
     }
 
     public function testString(UnitTester $I)
     {
-        $I->assertSame(CategoryType::ID_DATATYPE_STRING, FindDataType::handle('Data'));
+        $I->assertSame(Identifier::DATATYPE_STRING, FindDataType::handle('Data'));
     }
 }
