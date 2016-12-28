0b7f81c165c0173921edd6de3a519bca431381fc
iampersistent <develop@zestic.com>
2016-12-28 15:12:46 -0800

    combine find data type tests

--- tests/unit/Core/FindDataTypeCest.php
+++ tests/unit/Core/FindDataTypeCest.php
@@ -3,6 +3,7 @@ declare(strict_types = 1);
 
 use Cerberus\Core\Exception\DataTypeException;
 use Cerberus\Core\FindDataType;
+use Cerberus\Core\Identifier;
 
 class FindDataTypeCest
 {
@@ -12,6 +13,31 @@ class FindDataTypeCest
         $I->expectException($exception, function() {
             FindDataType::handle([]);
         });
+    }
+
+    public function testBoolean(UnitTester $I)
+    {
+        $I->assertSame(Identifier::DATATYPE_BOOLEAN, FindDataType::handle(false));
+    }
+
+    public function testDate(UnitTester $I)
+    {
+        $I->assertSame(Identifier::DATATYPE_DATETIME, FindDataType::handle(new DateTime()));
+    }
+
+    public function testDouble(UnitTester $I)
+    {
+        $I->assertSame(Identifier::DATATYPE_DOUBLE, FindDataType::handle(3.14));
+    }
+
+    public function testInteger(UnitTester $I)
+    {
+        $I->assertSame(Identifier::DATATYPE_INTEGER, FindDataType::handle(42));
+    }
+
+    public function testString(UnitTester $I)
+    {
+        $I->assertSame(Identifier::DATATYPE_STRING, FindDataType::handle('Data'));
     }
 
     public function testInvalidType(UnitTester $I)
