cfa63b0db52bdac8ec658abd17e75413ba5fb5ec
Ivan Deer <ruskiyos@gmail.com>
2017-01-16 15:43:03 -0800

    Use cerberus class name as singleton registry for DI resolution.

--- src/Facades/CerberusFacade.php
+++ src/Facades/CerberusFacade.php
@@ -3,12 +3,13 @@ declare(strict_types = 1);
 
 namespace Cerberus\Facades;
 
+use Cerberus\CerberusService;
 use Illuminate\Support\Facades\Facade;
 
 class CerberusFacade extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'cerberus';
+        return CerberusService::class;
     }
 }
